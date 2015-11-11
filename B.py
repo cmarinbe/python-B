fd26dfd1ef27337fbabffc3e0ca5a2be3980d25b
Kevin Dungs <kevin@dun.gs>
2015-11-11 14:39:29 +0100

    Extract data and plot mass.

---
+++ B.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+from collections import namedtuple
+import matplotlib.pyplot as plt
+
+
+B = namedtuple('B', ['m', 'merr', 'pt', 'p'])
+bs = []
+with open('B.txt') as f:
+    for line in f.readlines():
+        line = line.strip()
+        values = line.split(',')
+        actual_values = [float(v) for v in values]
+        b = B(*actual_values)
+        bs.append(b)
+
+masses = [b.m for b in bs]
+plt.hist(masses)
+plt.savefig('mass.pdf')
